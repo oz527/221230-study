472f2ceb1341655cc32153575ecf33f95ebdacb1
oz527 <118091285+oz527@users.noreply.github.com>
2022-12-30 03:16:48 +0900

    study

---
+++ app.js
@@ -0,0 +1,9 @@
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
